caabc50584a071017c68b228c33528eeaf052435
Shoukugeki02 <karn@eng.cmu.ac.th>
2020-01-21 17:05:32 +0700

    lap7

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -6,12 +7,23 @@ int main()
   char rank;
   cout << "Input your rank:";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
-  return 0;
+  if(rank == 83){
+    cout << "you have received Super Ultra Rare Unit!!!\n";
+    cout << "You have received 5 gems.\n";
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";}
+	else if(rank==65){cout << "You have received 5 gems.\n";
+	cout << "You have received 1 gems.\n";
+	cout << "You have received 2000 coins.\n";
+	cout << "You have received very KAK items.\n";}
+    else if(rank==66){cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";}
+	else if(rank==67){cout << "You have received 2000 coins.\n";
+	cout << "You have received very KAK items.\n";}	
+	else if(rank==68){cout << "You have received very KAK items.\n";}
+
+
+	return 0;
 }
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,63 @@
-//status = "ONE-NIGHT-STAND";
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int age;
+	float tall;
+	int money;
+	string status;
+	cout << "Enter your age : ";
+	cin >> age;
+	if(age < 20)
+	{
+		cout << "Enter your tall : ";
+		cin >> tall;
+		if(tall < 160)
+		{
+			status = "UNFRIEND";
+		}
+		else
+		{
+			if(tall < 175)
+			{
+				status = "FRIEND";
+			}
+			else
+			{
+				cout << "Enter your money : ";
+				cin >> money;
+				if(money >= 69000000)
+				{
+					status = "MARRIED";
+				}
+				else
+				{
+					status = "ONE NIGHT STAND";
+				}
+			}
+		}
+	}
+	else
+	{
+		if(age < 30)
+		{
+			cout << "Enter your money : ";
+			cin >> money;
+			if(money > 10000000)
+			{
+				status = "BEST FRIEND";
+			}
+			else
+			{
+				status = "UNFRIEND";
+			}
+		}
+		else
+		{
+			status = "UNFRIEND";
+		}
+	}
+	cout << status;
+
+}
